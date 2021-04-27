ac31b694586d33b2cdf046c4ca0c151206515aeb
Bas Visscher <bas.visscher24@gmail.com>
2021-04-27 12:01:00 +0200

    Added project

---
+++ SiglentScope/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace SiglentScope
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            FRMLib.Scope.ScopeViewSettings scopeViewSettings1 = new FRMLib.Scope.ScopeViewSettings();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.scopeView1 = new FRMLib.Scope.Controls.ScopeView();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(371, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(353, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "192.168.11.55:5024";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(452, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // scopeView1
+            // 
+            this.scopeView1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.scopeView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.scopeView1.DataSource = null;
+            this.scopeView1.Location = new System.Drawing.Point(12, 39);
+            this.scopeView1.Name = "scopeView1";
+            scopeViewSettings1.BackgroundColor = System.Drawing.Color.Black;
+            scopeViewSettings1.DrawScalePosHorizontal = FRMLib.Scope.DrawPosHorizontal.Bottom;
+            scopeViewSettings1.DrawScalePosVertical = FRMLib.Scope.DrawPosVertical.Right;
+            scopeViewSettings1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            scopeViewSettings1.GridZeroPosition = FRMLib.Scope.VerticalZeroPosition.Middle;
+            scopeViewSettings1.HorizontalDivisions = 10;
+            scopeViewSettings1.HorOffset = 0D;
+            scopeViewSettings1.HorScale = 10D;
+            scopeViewSettings1.NotifyOnChange = true;
+            scopeViewSettings1.VerticalDivisions = 8;
+            scopeViewSettings1.ZeroPosition = FRMLib.Scope.VerticalZeroPosition.Middle;
+            this.scopeView1.Settings = scopeViewSettings1;
+            this.scopeView1.Size = new System.Drawing.Size(599, 371);
+            this.scopeView1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.scopeView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private FRMLib.Scope.Controls.ScopeView scopeView1;
+    }
+}
+
---
+++ SiglentScope/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Siglent;
+using STDLib.Ethernet;
+
+namespace SiglentScope
+{
+    public partial class Form1 : Form
+    {
+        SDS1104 siglent = new SDS1104();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await siglent.ConnectAsync(textBox2.Text);
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            await siglent.GetWaveForm(SDS1104.Channels.C3);
+        }
+    }
+
+
+    
+
+
+
+}
+
+namespace Siglent
+{
+    class SDS1104
+    {
+        SCPI scpi = new SCPI();
+
+        public SDS1104()
+        {
+
+        }
+
+        public async Task<bool> ConnectAsync(string host, CancellationTokenSource cts = null)
+        {
+            return await scpi.ConnectAsync(host, cts);
+        }
+
+
+
+        public async Task<double[]> GetWaveForm(Channels channel, CancellationTokenSource cts = null)
+        {
+            double vdiv = await GetVDIV(channel, cts);
+            double ofst = await GetOFST(channel, cts);
+
+            //string description = await scpi.SendCommand($"{channel}:WaveForm? DESC", cts);
+
+            byte[] wave = await scpi.SendCommand(Encoding.ASCII.GetBytes($"{channel}:WaveForm? DAT2"), cts);
+
+            //Match blkLenM = Regex.Match(description, @"#.(\d+)");
+
+            //int blkLen = int.Parse(blkLenM.Groups[1].Value);
+
+            double[] res = new double[wave.Length];
+
+            for (int i = 0; i < wave.Length; i++)
+            {
+                double x = wave[i];
+                if (x > 127)
+                    x -= 255;
+
+                res[i] = x * (vdiv / 25) - ofst;
+            }
+
+            return res;
+        }
+
+
+
+
+
+        public async Task<double> GetVDIV(Channels channel, CancellationTokenSource cts = null)
+        {
+            return await GetDouble($"{channel}:VDIV?", cts);
+        }
+
+
+        public async Task<double> GetOFST(Channels channel, CancellationTokenSource cts = null)
+        {
+            return await GetDouble($"{channel}:OFST?", cts);
+        }
+
+
+        
+
+
+
+
+
+
+
+        public async Task<double> GetDouble(string cmd, CancellationTokenSource cts = null)
+        {
+            string res = await scpi.SendCommand(cmd, cts);
+            Match m = Regex.Match(res, @"(-?\d+)\.?\d+(E-|E\+|E|\d+)\d+");
+            return double.Parse(m.Value, NumberStyles.Number | NumberStyles.AllowExponent);
+        }
+        
+
+
+
+        public enum Channels
+        { 
+            C1,
+            C2,
+            C3,
+            C4
+        }
+
+
+    }
+
+
+    class SCPI
+    {
+        TcpSocketClient client;
+
+        public SCPI()
+        {
+            client = new TcpSocketClient();
+            client.OnDataRecieved += Client_OnDataRecieved;
+        }
+
+
+
+
+
+
+
+
+        public async Task<bool> ConnectAsync(string host, CancellationTokenSource cts = null)
+        {
+            return await client.ConnectAsync(host, cts);
+        }
+
+        TaskCompletionSource<byte[]> recieved;
+        List<byte> rxBuf = new List<byte>();
+        private void Client_OnDataRecieved(object sender, byte[] e)
+        {
+            rxBuf.AddRange(e);
+            if(rxBuf.Last() == 10)
+            {
+                if (recieved != null)
+                    recieved.TrySetResult(rxBuf.ToArray());
+                rxBuf.Clear();
+
+            }
+        }
+
+
+        public async Task<string> SendCommand(string cmd, CancellationTokenSource cts = null)
+        {
+            return Encoding.ASCII.GetString(await SendCommand(Encoding.ASCII.GetBytes(cmd)));
+        }
+
+
+        public async Task<byte[]> SendCommand(byte[] cmd, CancellationTokenSource cts = null)
+        {
+            byte[] result = null;
+
+            if (cts == null) (cts = new CancellationTokenSource()).CancelAfter(2500);
+            recieved = new TaskCompletionSource<byte[]>();
+            //cts.Token.Register(() => { recieved.TrySetCanceled(); });
+
+            rxBuf.Clear();
+            client.SendData(cmd);
+
+            try
+            {
+                result = await recieved.Task;
+            }
+            catch (Exception exc)
+            {
+
+            }
+
+            return result;
+        }
+
+
+        
+
+    }
+
+
+
+
+
+}
+
