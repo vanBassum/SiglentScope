44c59c170ab24b199a4a3ac97b5f5e8800e65708
Bas Visscher <bas.visscher24@gmail.com>
2022-01-19 19:32:07 +0100

    Started adding commands

---
+++ SiglentScope/SCPI/Devices/SDS1104XE/Commands/WF.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace SCPI.Devices.SDS1104XE.Commands
+{
+    public class WF : IQuery
+    {
+        public Status Status { get; set; }
+        public string Channel { get; set; } = "C1";
+
+
+
+
+
+        public string Query => $"{Channel}:WF?";
+        public bool Parse(byte[] data)
+        {
+            throw new NotImplementedException();
+            var str = Encoding.ASCII.GetString(data).Trim('\0', '\n').Split(' ').Last();
+            if (Convert.ToDecimal(str, out decimal value))
+            {
+                //Value = value;
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
+
---
+++ SiglentScope/SCPI/Devices/SDS1104XE/Commands/WFSU.cs
@@ -0,0 +1,28 @@
+﻿namespace SCPI.Devices.SDS1104XE.Commands
+{
+    public class WFSU : ICommand//, IQuery
+    {
+        public Status Status { get; set; }
+        public string Channel { get; set; } = "C1";
+        public decimal FirstPoint { get; set; } = 0;
+        public decimal Sparsing { get; set; } = 0;
+        public decimal SampleSize { get; set; } = 100;
+
+
+        public string Command => $"WFSU SP, {Sparsing}, NP, {SampleSize}, FP, {FirstPoint}";
+        //public string Query => $"{Channel}:VDIV?";
+        
+        //public bool Parse(byte[] data)
+        //{
+        //    var str = Encoding.ASCII.GetString(data).Trim('\0', '\n').Split(' ').Last();
+        //    if (Convert.ToDecimal(str, out decimal value))
+        //    {
+        //        Value = value;
+        //        return true;
+        //    }
+        //    return false;
+        //}
+    }
+}
+
+
